35df18ea3ad4b8788a629c869cb1237ac50a552b
rvarun-26 <rvarun604@gmail.com>
2026-01-19 15:34:52 +0530

    changes311

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+# 1. THE IMPORT BLOCK
+# This stops the "Resource already exists" error by adopting the existing RG
+import {
+  to = azurerm_resource_group.rg
+  id = "/subscriptions/d008c7cc-9795-4292-bf79-74ae52cda63d/resourceGroups/LabsKraft-Ted"
+}
+
+# 2. RESOURCE GROUP
 resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.location
@@ -6,6 +14,7 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
+# 3. STORAGE ACCOUNT
 resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
@@ -17,6 +26,7 @@ resource "azurerm_storage_account" "sa" {
   }
 }
 
+# 4. STORAGE CONTAINER
 resource "azurerm_storage_container" "container" {
   name                  = var.container_name
   storage_account_name  = azurerm_storage_account.sa.name
