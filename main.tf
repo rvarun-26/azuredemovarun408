40b57c0aab18e324e3f88dd5590c8ecb47d5998b
rvarun-26 <rvarun604@gmail.com>
2026-01-19 14:16:29 +0530

    changes31

--- main.tf
+++ main.tf
@@ -1,40 +1,24 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+  tags = {
+    env = var.tags
+  }
+}
 
-resource "azurerm_resource_group" "rg" { 
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags = {
+    env = var.tags
+  }
+}
 
-  name     = var.rg_name 
-
-  location = var.location 
-
-  tags     = var.tags 
-
-} 
-
- 
-
-resource "azurerm_storage_account" "sa" { 
-
-  name                     = var.storage_account_name 
-
-  resource_group_name      = azurerm_resource_group.rg.name 
-
-  location                 = azurerm_resource_group.rg.location 
-
-  account_tier             = "Standard" 
-
-  account_replication_type = "LRS" 
-
-  tags                     = var.tags 
-
-} 
-
- 
-
-resource "azurerm_storage_container" "container" { 
-
-  name                  = var.container_name 
-
-  storage_account_name  = azurerm_storage_account.sa.name 
-
-  container_access_type = "private" 
-
-} 
+resource "azurerm_storage_container" "container" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
--- variables.tf
+++ variables.tf
@@ -1,19 +1,9 @@
-variable "location" { type = string } 
-
-variable "rg_name"  { type = string } 
-
- 
-
-variable "storage_account_name" { type = string } 
-
-variable "container_name"       { type = string } 
-
- 
+variable "location"             { type = string }
+variable "rg_name"              { type = string }
+variable "storage_account_name" { type = string }
+variable "container_name"       { type = string }
 
 variable "tags" { 
-
-  type    = map(string) 
-
-  default = {} 
-
-} 
+  type    = string      # Changed from map(string) to string
+  default = "dev" 
+}
